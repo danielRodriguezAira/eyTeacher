94d587e0ae29bc67d80ac198890775594aa787f6
Daniel Rodriguez Aira <leinadfonfria@gmail.com>
2026-02-03 22:21:12 +0100

    Primer commit

---
+++ backend/eyTeacher/src/test/java/es/leinadfonfria/eyteacher/EyTeacherApplicationTests.java
@@ -0,0 +1,13 @@
+package es.leinadfonfria.eyteacher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EyTeacherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
